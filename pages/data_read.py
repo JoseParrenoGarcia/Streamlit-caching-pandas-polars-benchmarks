ee96e1ee40ecbcf36abd901249d9973ccd2538b1
Jose Parreño Garcia <jose.parreno@skyscanner.net>
2024-07-29 15:59:19 +0200

    Read data benchmarks
    * Added description to data_read.py

--- pages/data_read.py
+++ pages/data_read.py
@@ -12,6 +12,13 @@ st.set_page_config(
 pages_format()
 
 # ---------------------------------------------------------------------
+# DATA READ EXECUTION
+# ---------------------------------------------------------------------
+datasets = [1_000, 10_000, 100_000, 1_000_000, 10_000_000]
+
+
+
+# ---------------------------------------------------------------------
 # HOME PAGE - SIDEBAR
 # ---------------------------------------------------------------------
 with st.sidebar:
@@ -20,4 +27,12 @@ with st.sidebar:
 # ---------------------------------------------------------------------
 # HOME PAGE - MAIN CONTENT AREA
 # ---------------------------------------------------------------------
-st.write('hello2')
+st.html('<h5>Speed of data reads</h5>')
+st.write('In this page, we will compare how fast do pandas and polars read data. In addition, we want to see the effects that <i>st.cache_data()</i> has on'
+         ' the speed of execution when the app is re-ran.')
+st.markdown("- Unfortunately, Github does not allow to store data that exceeds 100MB. \n"
+            "- For datasets of 1m+ records, we would be hitting this limit. \n"
+            "- Until further resolution, I have decided to create the files on the fly. \n"
+            "- In other words, the execution of the app might be slower because we have to run the synthetic generator code for big datasets. \n"
+            )
+
--- synthetic_data/synthetic_data_generator.py
+++ synthetic_data/synthetic_data_generator.py
@@ -3,8 +3,10 @@ import numpy as np
 from datetime import datetime, timedelta
 import os
 
+import streamlit
 
-def generate_dataset(num_rows, start_date, end_date, output_dir):
+
+def generate_dataset_pandas(num_rows, start_date=datetime(2023, 1, 1), end_date=datetime(2024, 12, 31)):
     # Generate random dates
     date_range = pd.date_range(start=start_date, end=end_date, periods=num_rows).date
 
@@ -31,26 +33,42 @@ def generate_dataset(num_rows, start_date, end_date, output_dir):
         'Device': device
     })
 
-    # Save as CSV and Parquet
-    csv_filename = os.path.join(output_dir, f'dataset_{num_rows}.csv')
-    parquet_filename = os.path.join(output_dir, f'dataset_{num_rows}.parquet')
-
-    df.to_csv(csv_filename, index=False)
-    df.to_parquet(parquet_filename, index=False)
-
-    print(f"Dataset with {num_rows} rows saved as CSV and Parquet.")
+    return df
 
 
-# Set parameters
-start_date = datetime(2023, 1, 1)
-end_date = datetime(2024, 12, 31)
-output_dir = 'data'
+@streamlit.cache_data()
+def generate_dataset_pandas(num_rows, start_date=datetime(2023, 1, 1), end_date=datetime(2024, 12, 31)):
+    # Generate random dates
+    date_range = pd.date_range(start=start_date, end=end_date, periods=num_rows).date
 
-# Create output directory if it doesn't exist
-os.makedirs(output_dir, exist_ok=True)
+    # Generate other columns
+    impressions = np.random.randint(0, 10_000_001, num_rows)
+    ctr = np.round(np.random.random(num_rows), 3)
+    clicks = (impressions * ctr).astype(int)
+    cpc = np.round(np.random.uniform(0.10, 2.35, num_rows), 3)
+    cost = np.round(clicks * cpc, 3)
+    roi = np.round(np.random.uniform(0.75, 1.55, num_rows), 3)
+    revenue = np.round(cost * roi, 3)
+    device = np.random.choice(['Desktop', 'Mobile'], num_rows)
 
-# Generate datasets
-datasets = [1_000, 10_000, 100_000, 1_000_000, 10_000_000]
+    # Create DataFrame
+    df = pd.DataFrame({
+        'Date': date_range,
+        'Impressions': impressions,
+        'CTR': ctr,
+        'Clicks': clicks,
+        'CPC': cpc,
+        'Cost': cost,
+        'ROI': roi,
+        'Revenue': revenue,
+        'Device': device
+    })
 
-for num_rows in datasets:
-    generate_dataset(num_rows, start_date, end_date, output_dir)
+    return df
+
+
+# # Generate datasets
+# datasets = [1_000, 10_000, 100_000, 1_000_000, 10_000_000]
+#
+# for num_rows in datasets:
+#     generate_dataset(num_rows, start_date, end_date, output_dir)
